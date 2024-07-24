6082661bffe5565723dd5d3dc08db6482e553adc
Cesia Arevalo <ustr027024@ugb.edu.sv>
2024-07-24 10:23:52 -0600

    Determinar el problema de una serie de numeros

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -10,7 +10,18 @@ namespace miPrimerProyecto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("mi primer proyecto");
+            Console.WriteLine("mi primer proyecto");//prioridad de los operadores aritmeticos.
+                                                    //ejercicio obtener el promedio de una serie de numeros
+            int[] serie = new int[] { 5, 4, 6, 8, 9 }; //32
+            int suma = 0;
+            foreach (int num in serie)
+            {
+                suma = suma + num;
+            }
+            decimal prom = suma / serie.Length;
+            Console.WriteLine("La suma es: {0}, el promedio {1}", suma, prom);
+            //pausa.
+            Console.ReadLine();
         }
     }
 }
