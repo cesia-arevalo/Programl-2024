9f09b2408b8ce91bf5238065d4b4f457e17ea723
cesia-arevalo <ustr027024@ugb.edu.sv>
2024-08-06 18:16:45 -0500

    prioridad de los operadores aritméticos

--- mi PrimerProyecto/mi PrimerProyecto/Program.cs
+++ mi PrimerProyecto/mi PrimerProyecto/Program.cs
@@ -11,14 +11,22 @@ namespace mi_PrimerProyecto
         static void Main(string[] args{
 
 
-            Console.WriteLine ( " Num1 : " ) ;
-            int num1 = int .Parse ( Console.ReadLine ( ) ) ;
+            bool comprobar = true ;
+            Console . WriteLine ( " El valor es : {0} " , comprobar ) ;
+            //prioridad de los operadores aritmeticos.
+            //se pone el sufijo m para la división con decimales.
+             decimal respuesta = 5 + 8 - 2 * 5 / 4m + 1; //14.75 valor incorrecto; 11.5 -> valor correcto
+            Console . WriteLine ( " La respuesta es: {0} " , respuesta ) ;
 
-            Console.WriteLine ( " Num 2 : " ) ;
-            int num2 = int . Parse ( Console.ReadLine ( ) ) ;
+            Console . WriteLine ( " Nombre: " ) ;
+            string nombre = Console.ReadLine ( ) ;
 
-            int respuesta = num1 + num2 ;
-            Console . WriteLine ( " La suma de {0} + {1} = {2} " , num1 , num2 , respuesta ) ;
+            Console . WriteLine ( " Hola {0} bienvenido " , nombre ) ;
+
+            DateTime fechaActual = new DateTime ( ) ;
+            fechaActual = DateTime . Now ;
+            Console . WriteLine ( " La fecha actal es: {0} " , fechaActual ) ;
+            Console . WriteLine ( " La división de 5/4 es {0}  " , 5/4 ) ;
 
             //Pausa.
             Console .ReadLine ( ) ;
