5c71575e9a7f871bf96c17a1f95fa2b0d07d0dbc
cesia-arevalo <ustr027024@ugb.edu.sv>
2024-08-06 20:38:04 -0500

    Nota final de computo 1 y 2

--- mi PrimerProyecto/mi PrimerProyecto/Program.cs
+++ mi PrimerProyecto/mi PrimerProyecto/Program.cs
@@ -11,28 +11,21 @@ namespace mi_PrimerProyecto
         static void Main(string[] args{
 
 
-            //prioridad de los operadores aritméticos.
-            // se pone el sufilo m para la division con decimales .
-             decimal respuesta = 5 + 8 - 2 * 5 / 4m + 1; //14.75 valor incorrecto; 11.5 -> valor correcto
-            Console . WriteLine ( " La respuesta es: {0} " , respuesta ) ;
-            // ejercicio de obtener la nota final de la materia de programacion I .
-            Console.WriteLine ( " Lab1 : " ) ;
-            double lab1 = double . Parse ( Console . ReadLine ( ) ) ; //8
+            double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+            Console . WriteLine(" La nota de C1 es: {0} ", c1);
 
-            Console.WriteLine ( " Lab2 : " ) ;
-            double lab2 = double . Parse ( Console . ReadLine ( ) ) ; //9
+            Console.WriteLine ( " Lab1 : " ) ;​
+            lab1 = double . Parse ( Console . ReadLine ( ) ) ;  //8
 
-            Console . WriteLine ( " La división de 5/4 es {0} " , 5/4 ) ;
-            Console . WriteLine ( " parcial 1: " ) ;
-            double parcial1 = double . Parse ( Console.ReadLine ( ) ) ; //7
+            Console.WriteLine ( " Lab2 : " ) ;​
+            lab2 = double . Parse ( Console . ReadLine ( ) ) ;  //9
 
-            // 8*30%= 2,4
-            // 9*30%= 2,7
-            // 7*40%= 2,8
-            //C1 = 7,9
+            Console . WriteLine ( " Parcial 1: " ) ;
+            parcial1 = double . Parse ( Console . ReadLine ( ) ) ;  //7
 
-            double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100 ;
-            Console . WriteLine ( " La nota de c1 es: {0} " , c1 ) ;
+            double c2 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100 ;
+            Console . WriteLine ( " La nota de C2 es: {0} " , c2 ) ;
+
             //Pausa.
             Console .ReadLine ( ) ;
         }
