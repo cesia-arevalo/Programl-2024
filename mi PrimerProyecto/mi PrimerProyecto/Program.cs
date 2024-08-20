e25031e8d236e985d2f0bce38b32ff3b15394f5b
cesia-arevalo <ustr027024@ugb.edu.sv>
2024-08-19 23:19:50 -0500

    Uso de matriz bidimensional

--- mi PrimerProyecto/mi PrimerProyecto/Program.cs
+++ mi PrimerProyecto/mi PrimerProyecto/Program.cs
@@ -8,12 +8,19 @@ namespace mi_PrimerProyecto
 {
     class Program
     {
-        static void Main(string[] args) { 
-            //Uso de matrices
-            int[] numeros = new int[] { 2, 8, 9, 10, 1, };
-            for (int i = 0; i < numeros.Length; i++)
+        static void Main(string[] args) {
+            //Uso de matrices 
+            int[,] matriz = new int[,] {
+                {5,4,5,2,1},
+                {1,2,8,9,3}
+            };
+            for (int i = 0; i < 2; i++)
             {
-                Console.WriteLine("Indice: {0}={1}", i, numeros[i]);
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine(matriz[i, j]);
+                }
+                Console.WriteLine("\n");
             }
             //pausa
             Console.ReadKey();
